38e1a7a1c2404379449ef7d6a70bdfd1529772f4
Spokik <Tackesi@yandex.ru>
2020-09-02 00:28:43 +0300

    Сттата в день по составам пользователя

--- Scan.js
+++ Scan.js
@@ -42,7 +42,8 @@
   function addBattons() {
     let footer = document.createElement("div")
     footer.className = "b-feedback"
-    footer.innerHTML = `Тут будет информация о загруженных за день файлых <div id="activeStat" class="tampleteButton" style="height:23px">Статистика</div>`;
+    let xc = sostavPerDay(usersSettings.usersSS)
+    footer.innerHTML = `${xc} <div id="activeStat" class="tampleteButton" style="height:23px">Статистика</div>`;
 
     let classBlok = document.createElement("style")
     classBlok.innerHTML = "div.tampleteButton{height: 30px;width: 130px;-webkit-appearance: button;-webkit-writing-mode: horizontal-tb !important;color: buttontext;text-shadow: none;display: inline-block;text-align: center;align-items: flex-start;background-color: buttonface;box-sizing: border-box;margin: 0em;font: 400 13.3333px Arial;padding: 1px 6px;border-width: 2px;border-style: outset;border-color: buttonface;border-image: initial;}}";
@@ -309,12 +310,10 @@
           } else {
             preSet[x.name] = ""
           }
-
-          //Не забудь про составы
+          //! Сделать меню выбора состава
+          preSet.usersSS = [5, 10]
         }
         localStorage.setItem(`usersSettings`, JSON.stringify(preSet))
-        console.log(`in localSorage:`)
-
       })
 
 
@@ -386,8 +385,6 @@
             docId: docId,
             a40: a40,
           }
-
-          console.log(`получены:`, params);
           resolve(params)
 
         } else if (mutationRecords.length === 1) {
@@ -403,7 +400,7 @@
 
         //Срабатывает прии НАЖАТИИ на "Сохранить"
         params.saveScanStat.onclick = () => {
-          let now = new Date();
+          let now = new Date()
           let dataForLS = {}
           dataForLS = JSON.parse(localStorage.getItem(`sostav${params.sostav}`))
           if (dataForLS == null) {
@@ -419,5 +416,28 @@
         }
       })
   }
+  //тут будет функция подсчитывающая сделанные составы в день
+  function sostavPerDay(sostavs) {
+    let today = new Date().getDate()
+    let month = new Date().getMonth() + 1
+    let counter = {}
+    for (let index = 0; index < sostavs.length; index++) {
+      let starArray = JSON.parse(localStorage.getItem(`sostav${sostavs[index]}`))
+      for (let key in starArray) {
+        if (starArray[key].month == month) {
+          if (starArray[key].day == today) {
+            counter[`sostavs${sostavs[index]}`] + 1
+          }
+        }
+      }
+    }
+    //string stringify
+    let stringReturn
+    for (let key in counter) {
+      stringReturn = stringReturn + `${key} состав: ${counter[key]} штук | `
+    }
+    return stringReturn
+  }
+
 
 })();
